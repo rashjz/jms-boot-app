01999b1464968f746093790f4c5af41f4c18910b
Rashad Javadov <rashjz@users.noreply.github.com>
2018-06-19 16:29:23 +0200

    Update JmsBootAppApplication.java

--- src/main/java/jms/boot/app/JmsBootAppApplication.java
+++ src/main/java/jms/boot/app/JmsBootAppApplication.java
@@ -2,23 +2,10 @@ package jms.boot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
-
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
 
 @EnableJms
-@ComponentScan(basePackages = "jms.boot.app")
 @EnableJpaRepositories
 @SpringBootApplication
 public class JmsBootAppApplication {
@@ -29,31 +16,6 @@ public class JmsBootAppApplication {
 //            repository.save(Order.builder().address("S Asgarova 103").name("Rashad Javadov").orderId(Long.valueOf(1234)).build());
 //        };
 //    }
-
-
-    @Bean
-    public JmsListenerContainerFactory<?> myFactory(
-            ConnectionFactory connectionFactory,
-            DefaultJmsListenerContainerFactoryConfigurer configurer) {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-
-        factory.setErrorHandler(t -> System.err.println("An error has occurred in the transaction " + t.getMessage()));
-
-        configurer.configure(factory, connectionFactory);
-        return factory;
-    }
-
-    @Bean
-    public MessageConverter jacksonJmsMessageConverter() {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setTargetType(MessageType.TEXT);
-        return converter;
-    }
-
-    @Bean
-    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) throws JMSException {
-        return new JmsTemplate(connectionFactory);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(JmsBootAppApplication.class, args);
