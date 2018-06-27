b76e5b96e8666013f36ff51431d7c4ec4b5ad3db
Rashad Javadov <rashjz@users.noreply.github.com>
2018-06-27 13:35:01 +0200

    Update OrderTransactionControllerTest.java

--- src/test/java/jms/boot/app/controller/OrderTransactionControllerTest.java
+++ src/test/java/jms/boot/app/controller/OrderTransactionControllerTest.java
@@ -2,6 +2,7 @@ package jms.boot.app.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jms.boot.app.domain.Order;
+import jms.boot.app.exception.OrderNotFoundException;
 import jms.boot.app.repository.OrderTransactionRepository;
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,9 +17,12 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.nio.charset.Charset;
+
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -28,8 +32,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class OrderTransactionControllerTest {
+    private static final String ORDER_NOT_FOUND_MESSAGE = "Requested order not found";
 
-    private String DESTINATION = "OrderTransactionQueue";
+    private static final String ILLEGAL_ARG_EXP_MSG = "This should be application specific";
+    private static final String DESTINATION = "OrderTransactionQueue";
+    private static final String ORDER_NAME = "TestOrderName";
+    private static final MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
 
     @MockBean
     private OrderTransactionRepository repository;
@@ -42,7 +52,7 @@ public class OrderTransactionControllerTest {
 
     @MockBean
     private JmsTemplate jmsTemplate;
- 
+
     @Test
     public void testSuccessDeleteMethod() throws Exception {
         doNothing().when(repository).delete(expectedOrder());
@@ -50,7 +60,7 @@ public class OrderTransactionControllerTest {
 
 
         mockMvc.perform(delete("/transaction/deleteOrder")
-                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(contentType)
                 .accept(MediaType.ALL)
                 .content(objectMapper.writeValueAsString(expectedOrder())))
                 .andExpect(status().isOk());
@@ -58,19 +68,56 @@ public class OrderTransactionControllerTest {
     }
 
     @Test
-    public void sendTest() throws Exception {
+    public void testDeleteMethodThrowExceptionNotFound() throws Exception {
+        doThrow(new OrderNotFoundException("No specific Order", 404)).when(repository).delete(expectedOrder());
+
+        mockMvc.perform(delete("/transaction/deleteOrder")
+                .contentType(contentType)
+                .accept(MediaType.ALL)
+                .content(objectMapper.writeValueAsString(expectedOrder())))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void sendTestCheckSuccessResult() throws Exception {
 //        given(createClientServiceMock.createClient("Foo")).willReturn(new Client("Foo"));
 
         doNothing().when(jmsTemplate).convertAndSend(DESTINATION, expectedOrder());
 
         mockMvc.perform(post("/transaction/send")
-                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(contentType)
                 .accept(MediaType.ALL)
                 .content(objectMapper.writeValueAsString(expectedOrder())))
                 .andExpect(status().isOk());
         verify(jmsTemplate, times(1)).convertAndSend(DESTINATION, expectedOrder());
 
 
+    }
+
+    @Test
+    public void testGetOrdersByOrderNameThrowIllegalArgumentException() throws Exception {
+
+        doThrow(new IllegalArgumentException()).when(repository).findByName(ORDER_NAME);
+
+        mockMvc.perform(get("/transaction/order/" + ORDER_NAME)
+                .contentType(contentType)
+                .accept(MediaType.ALL)
+                .content(objectMapper.writeValueAsString(expectedOrder())))
+                .andExpect(status().isConflict())
+                .andExpect(content().string(ILLEGAL_ARG_EXP_MSG));
+    }
+
+    @Test
+    public void testGetOrdersByOrderNameThrowOrderNotFoundException() throws Exception {
+
+        doThrow(new OrderNotFoundException(ORDER_NOT_FOUND_MESSAGE, -1)).when(repository).findByName(ORDER_NAME);
+
+        mockMvc.perform(get("/transaction/order/" + ORDER_NAME)
+                .contentType(contentType)
+                .accept(MediaType.ALL)
+                .content(objectMapper.writeValueAsString(expectedOrder())))
+                .andExpect(status().isConflict())
+                .andExpect(content().string(ORDER_NOT_FOUND_MESSAGE));
     }
 
     public Order expectedOrder() {
