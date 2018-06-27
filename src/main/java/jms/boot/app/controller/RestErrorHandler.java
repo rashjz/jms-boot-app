2b1e603d0589d5353743d82570cd6d205881d2eb
Rashad Javadov <rashjz@users.noreply.github.com>
2018-06-27 13:34:18 +0200

    Update RestErrorHandler.java

--- src/main/java/jms/boot/app/controller/RestErrorHandler.java
+++ src/main/java/jms/boot/app/controller/RestErrorHandler.java
@@ -2,18 +2,37 @@ package jms.boot.app.controller;
 
 import jms.boot.app.exception.OrderNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
 
 @Slf4j
 @ControllerAdvice
-public class RestErrorHandler {
+public class RestErrorHandler extends ResponseEntityExceptionHandler {
+
+    private static final String ILLEGAL_ARGUMENT_MESSAGE = "This should be application specific";
+    private static final String ACCESS_DENIED_MESSAGE = "Access denied message here";
 
     @ExceptionHandler(OrderNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public void handleTodoNotFoundException(OrderNotFoundException ex) {
+    public ResponseEntity<?> handleOrderNotFoundException(OrderNotFoundException ex, WebRequest request) {
         log.debug("handling 404 error on a order entry" + ex.getMessage());
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
+
+    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ILLEGAL_ARGUMENT_MESSAGE, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<Object> handleAccessDeniedException(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ACCESS_DENIED_MESSAGE, new HttpHeaders(), HttpStatus.FORBIDDEN);
+    }
+
 }
